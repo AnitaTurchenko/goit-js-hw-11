7775fbb0f7a9b9b141c54d4d25e82adea66516a0
Anita <dolar2007@ukr.net>
2023-08-16 14:48:21 +0300

    j

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { fetchUrl, renderMarkup,clearMarkup } from "./js/main_code.js";
+import { fetchUrl } from "./js/main_code.js";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import "./css/loader.css";
 import "simplelightbox/dist/simple-lightbox.min.css";
-// import axios from 'axios';
-let throttle = require('lodash.throttle');
-// const axios = require('axios').default;
+import SimpleLightbox from 'simplelightbox';
 
+let throttle = require('lodash.throttle');
 const API_KEY = '38824644-118675bee850a06ea8955044c';
 const BASE_URL = 'https://pixabay.com/api/';
+let gallery = null;
 
 const refs = {
     form: document.querySelector('.search-form'),
@@ -22,8 +22,55 @@ refs.loadBtn.addEventListener('click', onLoadMore);
 refs.loadBtn.classList.add('is-hidden');
 refs.alertLoader.classList.add('is-hidden');
 
+const optionsSet = {
+    captionDelay: 250,
+    captionsData: 'alt',
+    animationSpeed: 300,
+    swipeTolerance: 50,
+    fadeSpeed: 300,
+    scrollZoomFactor: 0.1,
+}  
     
- 
+ function renderMarkup(dataArray) {
+    if (dataArray.length === 0) {
+        return;
+    }
+    const markup = dataArray.map(({ webformatURL,likes,views,comments,downloads,tags,largeImageURL }) => 
+        `<a href="${largeImageURL}" class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" " />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</a>`
+    ).join('');
+    if (refs.loadBtn.classList.contains('is-hidden')) {
+        refs.loadBtn.classList.remove('is-hidden');
+    }
+    refs.gallery.insertAdjacentHTML("beforeend", markup);
+   
+    if (page === 1) {
+        gallery = new SimpleLightbox('.gallery a', optionsSet);
+    }
+    else{
+        gallery.refresh();
+    }
+    
+}
+
+function clearMarkup() {
+    return refs.gallery.innerHTML = "";
+}
 
     
 Notify.init({
